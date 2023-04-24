414dfd12b7cb66f2272339d66baffa6e16ad9602
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-04-24 15:11:30 -0700

    navbar init

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FiSearch, FiUser } from "react-icons/fi";
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <FiSearch />
+        </div>
+        <div className="navbar-center">The WSS</div>
+        <div className="navbar-end">
+          <span className="mr-2">
+            <FiUser />
+          </span>
+          Account
+        </div>
+      </div>
+      <div className="mb-4 w-full border-t-2"></div>
+      <div className="width-full flex justify-center gap-4">
+        <a href="">Footwear</a>
+        <a href="">Jerseys</a>
+        <a href="">Accessories</a>
+        <a href="">Clothing</a>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
---
+++ src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, NextScript, Main } from "next/document";
+
+export default function Document() {
+  return (
+    <Html data-theme="business">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
