117f17c3ec217516fcbd5da2b81b6d5afca6d54d
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-04-25 18:50:49 -0700

    added mobile for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,20 +13,10 @@ const Home: NextPage = () => {
       <Hero />
       <FeaturedCarousel />
       <div className="flex w-full flex-col items-center bg-base-200 py-12">
-        <h1 className="mb-2 text-2xl font-bold">
+        <h1 className="mx-2 mb-2 text-2xl font-bold">
           Follow Products and Updates on Instagram
         </h1>
-        <div className="flex gap-4">
-          <img
-            src="https://kiiaaunaenthemzngrew.supabase.co/storage/v1/object/public/wss.assests/4987cdc3-a063-4eb9-9371-05af6fc0d4be.webp"
-            alt=""
-            className="w-24"
-          />
-          <img
-            src="https://kiiaaunaenthemzngrew.supabase.co/storage/v1/object/public/wss.assests/4987cdc3-a063-4eb9-9371-05af6fc0d4be.webp"
-            alt=""
-            className="w-24"
-          />
+        <div className="flex flex-wrap gap-4 ">
           <img
             src="https://kiiaaunaenthemzngrew.supabase.co/storage/v1/object/public/wss.assests/4987cdc3-a063-4eb9-9371-05af6fc0d4be.webp"
             alt=""
