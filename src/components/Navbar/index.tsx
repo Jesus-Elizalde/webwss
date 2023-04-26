3d3a5a5c97eb73b3cd64d507c3abf5ab2289507c
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-04-26 09:47:15 -0700

    added google auth init

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,8 +1,12 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { FiSearch, FiUser } from "react-icons/fi";
 
 const NavBar = () => {
+  const { data: sessionData } = useSession();
+  console.log("🚀 ~ file: index.tsx:8 ~ NavBar ~ sessionData:", sessionData);
+
   return (
     <>
       <div className="navbar bg-base-100">
@@ -12,12 +16,46 @@ const NavBar = () => {
         <div className="navbar-center">
           <Link href="/">The WSS</Link>
         </div>
-        <div className="navbar-end">
-          <span className="mr-2">
-            <FiUser />
-          </span>
-          Account
-        </div>
+        {!sessionData?.user ? (
+          <div className="navbar-end" onClick={() => signIn()}>
+            <span className="mr-2">
+              <FiUser />
+            </span>
+            Sign In
+          </div>
+        ) : (
+          <div className="navbar-end">
+            <div className="dropdown-end dropdown">
+              <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
+                <div className="w-10 rounded-full">
+                  <img
+                    src={sessionData?.user?.image ?? ""}
+                    alt={sessionData?.user?.name ?? ""}
+                    // fill
+                    className="rounded-full"
+                  />
+                </div>
+              </label>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
+              >
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li onClick={() => void signOut()}>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
       <div className="mb-4 w-full border-t-2"></div>
       <div className="mb-10 flex w-full justify-center gap-4">
