892f8dcd162c478a5aafbed3f7406c0922167205
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-04-26 17:26:35 -0700

    updated prisma schema

---
+++ src/server/api/routers/product.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+export const productRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.product.findMany();
+  }),
+});
