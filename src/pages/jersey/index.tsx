e09bd5c21a98105a28ce3a4ddd7bf736ea3eade7
Jesus-Elizalde <95773936+Jesus-Elizalde@users.noreply.github.com>
2023-05-03 15:07:04 -0700

    Update index.tsx

--- src/pages/jersey/index.tsx
+++ src/pages/jersey/index.tsx
@@ -5,27 +5,7 @@ const JerseyPage = () => {
   return (
     <div>
       <div className="grid max-[640px]:grid-cols-2 max-[640px]:gap-x-2 max-[640px]:gap-y-2 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-4">
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
-        <MainCard />
+        jerseys
       </div>
     </div>
   );
