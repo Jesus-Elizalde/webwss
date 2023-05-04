a2fea01520e72d4fd5dc715b34e54577df5e0940
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-05-03 18:52:00 -0700

    added a main card place holder

--- src/pages/accessories/index.tsx
+++ src/pages/accessories/index.tsx
@@ -1,21 +1,34 @@
 import { useRouter } from "next/router";
 import React from "react";
+import LoadingMainCard from "~/components/Card/LoadingMainCard";
 import MainCard from "~/components/Card/MainCard";
 import { api } from "~/utils/api";
 import { getCategoryFromPath } from "~/utils/getCategory";
 
 const AccessoryPage = () => {
   const { asPath: path } = useRouter();
-  const { data: produts } = api.product.getByCategory.useQuery({
-    category: getCategoryFromPath(path),
-  });
+  const { data: products, isLoading: loading } =
+    api.product.getByCategory.useQuery({
+      category: getCategoryFromPath(path),
+    });
 
   return (
     <div>
       <div className="grid max-[640px]:grid-cols-2 max-[640px]:gap-x-2 max-[640px]:gap-y-2 lg:grid-cols-3 lg:gap-x-4 lg:gap-y-4">
-        {produts?.map((product) => (
-          <MainCard product={product} key={product.id} />
-        ))}
+        {loading ? (
+          <>
+            <LoadingMainCard />
+            <LoadingMainCard />
+            <LoadingMainCard />
+            <LoadingMainCard />
+            <LoadingMainCard />
+            <LoadingMainCard />
+          </>
+        ) : (
+          products?.map((product) => (
+            <MainCard product={product} key={product.id} />
+          ))
+        )}
       </div>
     </div>
   );
