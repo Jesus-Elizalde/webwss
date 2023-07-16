81e564e69be6c2c73b87ff56a54ca42ce46fe65e
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-07-15 21:35:35 -0700

    continue to comment out old code

--- src/server/api/routers/tag.ts
+++ src/server/api/routers/tag.ts
@@ -1,6 +1,6 @@
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { Prisma } from "@prisma/client";
-import { z } from "zod";
+// import { z } from "zod";
 
 export const defaultTagSelect = Prisma.validator<Prisma.ProductTagSelect>()({
   id: true,
@@ -14,23 +14,23 @@ export const productTagRouter = createTRPCRouter({
     });
   }),
 
-  getOne: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-      const { id } = input;
-      return ctx.prisma.productTag.findUnique({
-        where: { id: +id },
-        select: defaultTagSelect,
-      });
-    }),
+  // getOne: protectedProcedure
+  //   .input(z.object({ id: z.string() }))
+  //   .query(({ ctx, input }) => {
+  //     const { id } = input;
+  //     return ctx.prisma.productTag.findUnique({
+  //       where: { id: +id },
+  //       select: defaultTagSelect,
+  //     });
+  //   }),
 
-  delete: protectedProcedure
-    .input(z.object({ id: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.prisma.productTag.delete({
-        where: {
-          id: +input.id,
-        },
-      });
-    }),
+  // delete: protectedProcedure
+  //   .input(z.object({ id: z.string() }))
+  //   .mutation(async ({ ctx, input }) => {
+  //     return ctx.prisma.productTag.delete({
+  //       where: {
+  //         id: +input.id,
+  //       },
+  //     });
+  //   }),
 });
