6a556af1a8e83fd19fbdde0a710425d955f723fc
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-08-09 11:42:45 -0700

    added the different product image to main card

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, NextScript, Main } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme="dark">
+    <Html data-theme="light">
       <Head />
       <body>
         <Main />
